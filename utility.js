7c5c756539a64dc63bb5eab7ea21d88fc27ffd0e
CorrecTT <thanakornkh63@nu.ac.th>
2022-07-24 19:21:51 +0700

    first commit

--- utility.js
+++ utility.js
@@ -4,9 +4,9 @@ const request = require("request-promise");
 const EXTERNAL_API=""; // put url
 const accessToken=""; // put access token
 const student = {
-  name: '', // replace with your full name.
+  name: 'ธนกร คำเกิด', // replace with your full name.
   age: 19, // put your age.
-  gender: '', // replace with your gender
+  gender: 'ไม่ระบุ', // replace with your gender
   department: 'CPE'
 };
 
